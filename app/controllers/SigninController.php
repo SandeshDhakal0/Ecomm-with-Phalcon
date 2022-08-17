59f7158b024b552638af3f951ae7ff8da2fc572c
dhakalsandesh0 <dhakalsandesh0@gmail.com>
2022-08-17 11:05:41 +0545

    staged

---
+++ app/controllers/SigninController.php
@@ -0,0 +1,11 @@
+<?php
+
+class SigninController extends ControllerBase
+{
+   
+    public function indexAction()
+    {
+        echo "You are logged in.";
+    }
+    
+}
--- public/index.php
+++ public/index.php
@@ -59,8 +59,8 @@ try {
      */
     $application = new \Phalcon\Mvc\Application($di);
 
-    echo $application->handle($_SERVER['REQUEST_URI'])->getContent();
-} catch (\Exception $e) {
-    echo $e->getMessage() . '<br>';
-    echo '<pre>' . $e->getTraceAsString() . '</pre>';
-}
+        echo $application->handle($_SERVER['REQUEST_URI'])->getContent();
+    } catch (\Exception $e) {
+        echo $e->getMessage() . '<br>';
+        echo '<pre>' . $e->getTraceAsString() . '</pre>';
+    }
