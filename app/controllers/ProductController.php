058032353dcd07d0bd8dc192ca4efc94b08c0daa
dhakalsandesh0 <dhakalsandesh0@gmail.com>
2022-08-25 17:07:27 +0545

    product table and form completed

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -3,44 +3,70 @@ use Phalcon\Mvc\Controller;
 use App\Models;
 class ProductController extends ControllerBase
 {
-   
+
     public function indexAction()
     {
+        $prod = new Product();
+        $result = $prod->allCategory();
+        $this->view->setVar('category', $result);
     }
 
     public function createAction()
     {
-        $product = new Product();
-        $brand = $this->request->getPost('brand');
-        $title = $this->request->getPost('title');
-        $product_image = $this->request->getPost('product_image');
-        $product_prize = $this->request->getPost('product_prize');
-        
-        $product->title = $title;
-        $product->blog_description = $blog_description;
+        if ($this->request->isAjax()) {
+            $product = new Product();
+            // $brand = $this->request->getPost('brand');
+            // $title = $this->request->getPost('title');
+            // $prod_price = $this->request->getPost('prod_price');
+            // $product_image = $this->request->getPost('image');
+            // $prod_description = $this->request->getPost('prod_description');
+            // $size = json_encode($this->request->getPost('size'));
+            // $cat_id = $this->request->getPost('cat_id');
+            $product->brand = $this->request->getPost('brand');
+            $product->title = $this->request->getPost('title');
+            ;
+            $product->prod_price = $this->request->getPost('prod_price');
+            $product->prod_description = $this->request->getPost('prod_description');
+            $product->size = json_encode($this->request->getPost('size'));
+            $product->cat_id = $this->request->getPost('cat_id');
+            $img = array();
+            $i = 0;
+            if ($this->request->hasFiles() == true) {
+                foreach ($this->request->getUploadedFiles() as $file) {
+                    $Name = preg_replace("/[^A-Za-z0-9.]/", '-', $file->getName());
+                    $FileName = "public/img/product/" . $Name;
+                    $img[$i++] = $Name;
+                    if (!$file->moveTo($FileName)) {
+                        $this->flashSession->warning("An error occurred while uploading the document.");
+                    }
+                }
+                $product->product_image = json_encode($img);
+            }
+            if ($product->save() === false) {
+                $messages = $product->getMessages();
 
-        if ($this->request->hasFiles() == true) {
-            foreach ($this->request->getUploadedFiles() as $file) {
-                $Name = preg_replace("/[^A-Za-z0-9.]/", '-', $file->getName());
-                $FileName = "public/img/blog/" . $Name;
-
-                if (!$file->moveTo($FileName)) {
-                    $this->flashSession->warning("An error occurred while uploading the document.");
+                $errorMsg = 'Not working';
+                foreach ($messages as $message) {
+                    $errorMsg . "{$message} <br>";
                 }
             }
-            $blog->blog_image = $Name;
-        }
-
-        if (!$blog->save()) {
-            $this->flashSession->error("The form is empty.");
-            $this->view->disable();
-            $this->response->redirect("blog");
-        }
-        else {
-            $this->flashSession->success('Blog data successfully updated.');
-
-            $this->response->redirect("blog");
+            else {
+                return $this->response->setJsonContent("success");
+            }
         }
     }
-    
+
+    public function showAction()
+    {
+        $product = Product::find();
+        $products = $product->toArray();
+        $finalproduct = array();
+        foreach ($products as $prod) {
+            $res = new Product();
+            $cat = $res->findCategoryById($prod['cat_id']);
+            $prod['cat_id'] = $cat['cat_name'] . '-' . $cat['cat_cat'];
+            $finalproduct[] = $prod;
+        }
+        $this->view->setVar('prod', $finalproduct);
+    }
 }
--- app/models/Product.php
+++ app/models/Product.php
@@ -1,6 +1,45 @@
 <?php
+use Phalcon\Validation;
+
+use Phalcon\Paginator\Adapter\QueryBuilder as PaginatorQueryBuilder;
 
 class Product extends \Phalcon\Mvc\Model
+
 {
-    
+    public $prod_id;
+    public $brand;
+    public $title;
+    public $prod_price;
+
+    public $product_image;
+
+    public $prod_description;
+
+    public $size;
+
+    public $cat_id;
+
+
+    public function initialize()
+    {
+
+    }
+    public function allCategory(){
+        $this->setSource('category');
+        $result = $this->find();
+        if($result){
+            return $result->toArray();
+        }else
+        return null;
+    }
+    public function findCategoryById($cat_id){
+        $this->setSource('category');
+        $result = $this->find('cat_id='.$cat_id);
+       
+        if($result){
+            return $result->toArray()[0];
+        }else
+        return null;
+    }    
+
 }
