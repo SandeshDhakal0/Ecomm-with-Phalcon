261dc5847be3effa804b52049f04ab80141ac1f2
dhakalsandesh0 <dhakalsandesh0@gmail.com>
2022-08-30 17:20:44 +0545

    frontend needs a lot of work

--- app/controllers/FrontController.php
+++ app/controllers/FrontController.php
@@ -3,20 +3,45 @@ use \Phalcon\Tag;
 use Phalcon\Mvc\Controller;
 use Phalcon\Mvc\View;
 use App\Models;
+use Phalcon\Paginator\Adapter\NativeArray;
+use Phalcon\Mvc\Model;
+
 
 class FrontController extends ControllerBase
 {
+    public function initialize()
+    {
+        $page = $_GET['page'] ?? 1;
+        $data = Product::find()->toArray();
+
+        $currentPage = $page;
+        $paginator = new NativeArray(
+        [
+            "data" => $data,
+            "limit" => 4,
+            "page" => $currentPage,
+        ]);
+           
+        $paginate = $paginator->paginate();
+        //  echo "<pre>";
+        //  var_dump($paginate);
+        //  echo "<pre>";
+        //  die();
+
+        $this->view->blog = $paginate;
+    }
    
     public function indexAction()
     {
-        // $this->view->pick('../front/layout/header'); 
-        
-        // $this->view->pick('../front/layout/footer');     
+        $prod = Product::find();
+        $this->view->setVar('product',$prod);
     }
 
 
     public function womensdressAction()
     {
+        $prod = Product::find();
+        $this->view->setVar('product',$prod);
         
     }
 
